75f0cd48cbbad960f9449f808adef2f9ae1e4068
Nikolas Georgaklis <ngeorgak@villanova.edu>
2022-06-27 23:27:38 -0700

    Implemented logout and persisting user session functions

---
+++ Instagram/HomeViewController.h
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.h
+//  Instagram
+//
+//  Created by Nikolas Georgaklis on 6/27/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HomeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
